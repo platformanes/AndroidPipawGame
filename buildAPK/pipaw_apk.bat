75d29aab60d3915a04e6a33f484d29c2101e6d6e
Rect <rectvv@gmail.com>
2013-12-04 10:50:46 +0800

    add build apk

---
+++ buildAPK/pipaw_apk.bat
@@ -0,0 +1,1 @@
+"D:\FB\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\adt"  -package -target apk -storetype pkcs12 -keystore a.p12 -storepass 1234 pipaw.apk androidPipawSample-app.xml androidPipawSample.swf alipay_plugin_20120428msp.apk UPPayPluginEx.apk -extdir .
